c84046f73ecbc9defbe1ef46e7632c573092c9ba
gabriel <gabrielproencaaraujo@gmail.com>
2023-04-11 08:51:42 -0300

    chore(todo projeto): docker configurado, boilerplate backend e frontend rodando

--- app/backend/build/routes/index.js
+++ app/backend/build/routes/index.js
@@ -3,9 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoginRoutes = exports.UserRoutes = exports.CPFRoutes = void 0;
-const CPF_routes_1 = __importDefault(require("./CPF.routes"));
-exports.CPFRoutes = CPF_routes_1.default;
+exports.LoginRoutes = exports.UserRoutes = void 0;
 const user_routes_1 = __importDefault(require("./user.routes"));
 exports.UserRoutes = user_routes_1.default;
 const login_routes_1 = __importDefault(require("./login.routes"));
--- app/backend/src/services/index.ts
+++ app/backend/src/services/index.ts
@@ -1,4 +1,3 @@
-import CPFService from './CPF/cpf.service';
 import UserService from './user/users.service'
 
-export {CPFService, UserService };
+export { UserService };
